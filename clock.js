1e237c83c4dd7a1a2c07fa20e2795318d2a90166
vikas bn <vikas.bn@razorthink.net>
2016-12-14 17:59:29 +0530

    clock added to audiopage and working

--- clock.js
+++ clock.js
@@ -49,36 +49,3 @@ function clocktime()
 //    setInterval(incrementMinute,60000);
 //     setInterval(incrementMinute,3600000);
 }
-var audio = new Audio();
-var audio1 = new Audio("/home/vikasbn/Downloads/sounds/clap.wav");
-var audio2 = new Audio("/home/vikasbn/Downloads/sounds/boom.wav");
-var audio3 = new Audio("/home/vikasbn/Downloads/sounds/hihat.wav");
-var audio4 = new Audio("/home/vikasbn/Downloads/sounds/kick.wav");
-var audio5 = new Audio("/home/vikasbn/Downloads/sounds/openhat.wav");
-var audio6 = new Audio("/home/vikasbn/Downloads/sounds/ride.wav");
-var audio7 = new Audio("/home/vikasbn/Downloads/sounds/tink.wav");
-
-audio_array=[audio1,audio2,audio3,audio4,audio5,audio6,audio7];
-function playaudio(event){
-	var input = event.which;
-    var curaudio=input-97;    
-    audio.pause();
-    audio.currentTime=0;
-//    document.getElementById(input+"").style.transform="scale(1.2,1.2)"; 
-//    document.getElementById(input+"").style.backgroundColor="#90b350";
-   
-    document.getElementById(input+"").classList.add("audio-names__element--active");
-    audio=audio_array[curaudio];
-    audio.play();
-    
-    setTimeout(removestyle(input), 250);
-    function removestyle(input){
-       return function(){
-        if(document.getElementById(input+"").classList.contains("audio-names__element--active"))
-           document.getElementById(input+"").classList.remove("audio-names__element--active"); 
-       }
-    }
-     
-}
-
-
